9c96862360ddb9355ff2622bbcdafb26be9e4618
teru01 <teru0x01.sheep@gmail.com>
2020-12-18 12:02:38 +0900

    TCPPacketを使って送信

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,7 +1,12 @@
+use crate::packet::TCPPacket;
 use anyhow::{Context, Error, Result};
+use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::tcp::{self, MutableTcpPacket, TcpPacket};
 use pnet::packet::Packet;
-use pnet::transport::{TransportReceiver, TransportSender};
+use pnet::transport::{
+    self, TransportChannelType, TransportProtocol, TransportReceiver, TransportSender,
+};
+use pnet::util;
 use std::collections::HashMap;
 use std::fmt::{self, Display};
 use std::net::{IpAddr, Ipv4Addr};
@@ -9,7 +14,7 @@ use std::net::{IpAddr, Ipv4Addr};
 const TCP_HEADER_SIZE: usize = 20;
 const TCP_DATA_OFFSET: u8 = 5;
 
-struct TCB<'a> {
+pub struct TCB {
     src_addr: Ipv4Addr,
     dest_addr: Ipv4Addr,
     src_port: u16,
@@ -21,20 +26,20 @@ struct TCB<'a> {
     recv_buffer: Vec<u8>,
     send_channel: TransportSender,
     recv_channel: TransportReceiver,
-    retransmission_map: HashMap<u32, RetransmissionHashEntry<'a>>,
+    retransmission_map: HashMap<u32, RetransmissionHashEntry>,
 }
 
-struct RetransmissionHashEntry<'a> {
-    packet: TcpPacket<'a>,
+struct RetransmissionHashEntry {
+    packet: TCPPacket,
 }
 
-impl<'a> RetransmissionHashEntry<'a> {
-    fn new(packet: TcpPacket<'a>) -> Self {
+impl RetransmissionHashEntry {
+    fn new(packet: TCPPacket) -> Self {
         Self { packet }
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 struct SendParam {
     unacked_seq: u32, //未ACK送信
     next: u32,        //次の送信
@@ -42,7 +47,7 @@ struct SendParam {
     initial_seq: u32, //初期送信seq
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 struct RecvParam {
     next: u32,
     window: u16,
@@ -82,41 +87,59 @@ impl Display for TcpStatus {
     }
 }
 
-impl<'a> TCB<'a> {
-    fn send_tcp_packet(&mut self, seq: u32, ack: u32, flag: u16, payload: &[u8]) -> Result<usize> {
-        let mut tcp_buffer = vec![0; TCP_HEADER_SIZE + payload.len()];
-        let mut tcp_packet =
-            MutableTcpPacket::owned(tcp_buffer).context("failed to create packet")?;
-        tcp_packet.set_source(self.src_port);
-        tcp_packet.set_destination(self.dest_port);
-        tcp_packet.set_sequence(seq);
-        tcp_packet.set_acknowledgement(ack);
-        tcp_packet.set_data_offset(TCP_DATA_OFFSET);
-        tcp_packet.set_flags(flag);
-        tcp_packet.set_window(self.recv_param.window);
-        tcp_packet.set_payload(payload);
-        tcp_packet.set_checksum(tcp::ipv4_checksum(
-            &tcp_packet.to_immutable(),
-            &self.src_addr,
-            &self.dest_addr,
-        ));
-        let sent_size = self.send_to(&tcp_packet.to_immutable())?;
-        let t = TcpPacket::new(tcp_packet.packet());
-        self.retransmission_map.insert(
-            seq,
-            RetransmissionHashEntry::new(tcp_packet.consume_to_immutable()),
-        );
-        Ok((sent_size))
+impl TCB {
+    pub fn new(src_addr: Ipv4Addr) -> Result<Self> {
+        let (sender, receiver) = transport::transport_channel(
+            65535,
+            TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
+        )?;
+        Ok(Self {
+            src_addr,
+            dest_addr: "127.0.0.1".parse().unwrap(),
+            src_port: u16::default(),
+            dest_port: u16::default(),
+            send_param: SendParam::default(),
+            recv_param: RecvParam::default(),
+            status: TcpStatus::Closed,
+            send_buffer: vec![0; 65535],
+            recv_buffer: vec![0; 65535],
+            send_channel: sender,
+            recv_channel: receiver,
+            retransmission_map: HashMap::new(),
+        })
     }
 
-    // TCPパケットを送信
-    fn send_to(&mut self, packet: &TcpPacket) -> Result<usize> {
-        let packet_buf = packet.packet();
-        let mut buffer = vec![0; packet_buf.len()];
-        buffer.copy_from_slice(packet_buf);
-        let new_packet = TcpPacket::new(&buffer).context("failed to create new TCP packet")?;
-        self.send_channel
-            .send_to(new_packet, IpAddr::V4(self.dest_addr))
-            .map_err(|e| Error::new(e))
+    pub fn send_tcp_packet(
+        &mut self,
+        seq: u32,
+        ack: u32,
+        flag: u8,
+        payload: &[u8],
+    ) -> Result<usize> {
+        let mut tcp_packet = TCPPacket::new();
+        tcp_packet.set_src(self.src_port);
+        tcp_packet.set_dest(self.dest_port);
+        tcp_packet.set_seq(seq);
+        tcp_packet.set_ack(ack);
+        tcp_packet.set_data_offset(TCP_DATA_OFFSET);
+        tcp_packet.set_flag(flag);
+        tcp_packet.set_window_size(self.recv_param.window);
+        tcp_packet.set_payload(payload);
+        tcp_packet.set_checksum(util::ipv4_checksum(
+            &tcp_packet.packet(),
+            8,
+            &[],
+            &self.src_addr,
+            &self.dest_addr,
+            IpNextHeaderProtocols::Tcp,
+        ));
+        let sent_size = self
+            .send_channel
+            .send_to(tcp_packet.clone(), IpAddr::V4(self.dest_addr))
+            .context(format!("failed to send: \n{}", tcp_packet))?;
+
+        self.retransmission_map
+            .insert(seq, RetransmissionHashEntry::new(tcp_packet));
+        Ok(sent_size)
     }
 }
